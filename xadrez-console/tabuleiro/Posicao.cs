330e66065977e2f83b51fcdaa624871407aa4f7f
Isaque Araújo Nogueira <isaquean2@gmail.com>
2020-11-28 11:47:33 -0300

    Implementando as restricoes de movimento

--- xadrez-console/tabuleiro/Posicao.cs
+++ xadrez-console/tabuleiro/Posicao.cs
@@ -11,7 +11,7 @@
             this.coluna = coluna;
         }
 
-        public void definirValores(int linha,int coluna)
+        public void definirValores(int linha, int coluna)
         {
             this.linha = linha;
             this.coluna = coluna;
